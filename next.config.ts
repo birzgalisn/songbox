77ca9925310a9a706adcd5d6646b68e714728bb6
Niks Birzgalis <hello@niksbirzgalis.com>
2025-01-14 01:27:41 +0200

    refactor: remove ESLint and TypeScript build error ignore settings

--- next.config.ts
+++ next.config.ts
@@ -3,18 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
 } satisfies NextConfig;
 
 export default nextConfig;
